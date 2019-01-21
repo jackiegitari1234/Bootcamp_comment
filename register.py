5eade25812946b0283237243b00b6fa3a529e5c3
Michael Mbugua <michaelngigi76@gmail.com>
2019-01-21 13:46:46 +0300

    [finishes #163356820] Add login functionality

--- register.py
+++ register.py
@@ -2,18 +2,28 @@ users = []
 
 class User():
 
-    def __init__(self, firstname,lastname,username,password):
-        self.firstname = "firstname"
-        self.lastname = "lastname"
-        self.username = "username"
-        self.password = "password"
-        self.lastLoggedIn = None
-        self.role = 3
-        self.loggedIn = False
 
-    def save(self):
-        users.append(self)
+    def save(self, data):
+        users.append(data)
+
+    @staticmethod 
+    def login(username, password):
+        
+        for user in users: 
+            # user exists
+            if user["username"] == username:
+                
+                if user["password"] == password:
+                    print("Signed in as {}".format(user["username"]))
+                else:
+                    print("Password for {} is incorrect".format(user["username"]))
+            else:
+                print("User does not exist")
+
+
+            
+     
+        
     
 
 
-    
